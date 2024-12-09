853fb717dc9fd2b4780d467b0580f7f2b4bdf2cd
Anshu Saxena <anshus5802@gmail.com>
2024-12-09 23:36:22 +0530

    ticket chip added

--- src/pages/Commingsoon/Commingsoon.jsx
+++ src/pages/Commingsoon/Commingsoon.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import "./Commingsoon.scss";
-import { Box, Button, Container, Grid2 } from "@mui/material";
+import { Box, Button, Container, Grid2, Stack } from "@mui/material";
 import { usePriceHook } from "../../hooks/usePriceHook";
 import {
   addorSubTicket,
@@ -11,7 +11,14 @@ import {
 } from "../Booking/BookingSlice";
 import { useDispatch, useSelector } from "react-redux";
 import Grid from "@mui/material/Grid2";
-import { Typo_Basefont, Typo_Subheading, Typo_Subtitle } from "../../components/Typo/Typo";
+import {
+  Typo_Basefont,
+  Typo_Chip,
+  Typo_Subheading,
+  Typo_Subtitle,
+} from "../../components/Typo/Typo";
+import { beer } from "../../assets";
+import Imgbox from "../../components/Imgbox/Imgbox";
 
 // const TicketView = ({
 //   id,
@@ -34,20 +41,49 @@ import { Typo_Basefont, Typo_Subheading, Typo_Subtitle } from "../../components/
 //   );
 // };
 
-
 const ts = {
   g50: "#ffffff",
   g100: "#535353",
   g200: "#1B1B1B",
   g300: "#121212",
-  p400: "#E32B7E"
-}
+  p400: "#E32B7E",
+};
 
-
-const TicketBtn = () => { 
+const TicketAddBtn = () => {
   return (
     <>
-      <Button sx={{ width: "fit-content", bgcolor: `${ts.p400}`, color:`${ts.g50}`, fontSize:16, paddingInline: 6, fontFamily:"Anton" }}>Add</Button>
+      <Box>
+        <Stack direction={"row"}>
+          <Button sx={{ padding: 0, margin: 0, minWidth: "fit-content" }}>
+            -
+          </Button>
+          <Button sx={{ padding: 0, margin: 0, minWidth: "fit-content" }}>
+            0
+          </Button>
+          <Button sx={{ padding: 0, margin: 0, minWidth: "fit-content" }}>
+            +
+          </Button>
+        </Stack>
+      </Box>
+    </>
+  );
+};
+
+const TicketBtn = () => {
+  return (
+    <>
+      <Button
+        sx={{
+          width: "fit-content",
+          bgcolor: `${ts.p400}`,
+          color: `${ts.g50}`,
+          fontSize: 16,
+          paddingInline: 6,
+          fontFamily: "Anton",
+        }}
+      >
+        Add
+      </Button>
     </>
   );
 };
@@ -67,40 +103,88 @@ const TicketView = ({
   const { formatPrice, calSingleTicketTotal } = usePriceHook();
   return (
     <>
-    <Container maxWidth="md">
-      <Box sx={{ border: 1, borderColor:`${ts.g100}`, borderRadius:1, bgcolor:`${ts.g200}`}}>
-        {/* Ticket price and Action */}
-        <Box sx={{ padding: 2, borderBottom: 1, borderColor:`${ts.g100}`, borderBottomStyle:"dashed"}}>
-          <Grid container spacing={0} alignItems={"center"}>
-            <Grid size={6}>{/* Ticket Content */}
-              <Box sx={{bgcolor:`${ts.p400}`, width:"fit-content", marginBottom: 1.8}}>
-                <Typo_Subheading text="VIP" fs="1.8rem" fw="600"/>
-              </Box>
-              <Box sx={{marginBottom: 0.2}}>
-                <Typo_Subheading text="499/-" fs="1.8rem"/>
-              </Box>
-              <Box sx={{}}>
-                <Typo_Basefont text="Mobile ticket | Valid for 1 Person" fs="1.4rem" fw="300"/>
-              </Box>
+      <Container maxWidth="md">
+        <Box
+          sx={{
+            border: 1,
+            borderColor: `${ts.g100}`,
+            borderRadius: 1,
+            bgcolor: `${ts.g200}`,
+          }}
+        >
+          {/* Ticket price and Action */}
+          <Box
+            sx={{
+              padding: 2,
+              borderBottom: 1,
+              borderColor: `${ts.g100}`,
+              borderBottomStyle: "dashed",
+            }}
+          >
+            <Grid container spacing={0} alignItems={"center"}>
+              <Grid size={6}>
+                {/* Ticket Content */}
+                <Box
+                  sx={{
+                    bgcolor: `${ts.p400}`,
+                    width: "fit-content",
+                    marginBottom: 1.8,
+                  }}
+                >
+                  <Typo_Subheading text="VIP" fs="1.8rem" fw="600" />
+                </Box>
+                <Box sx={{ marginBottom: 0.2 }}>
+                  <Typo_Subheading text="499/-" fs="1.8rem" />
+                </Box>
+                <Box sx={{}}>
+                  <Typo_Basefont
+                    text="Mobile ticket | Valid for 1 Person"
+                    fs="1.4rem"
+                    fw="300"
+                  />
+                </Box>
+              </Grid>
+              <Grid size={6}>
+                {/* Ticket Action Btn */}
+                <Box sx={{ float: "right" }}>
+                  <TicketBtn />
+                  {/* <TicketAddBtn /> */}
+                </Box>
+              </Grid>
             </Grid>
-            <Grid size={6}>{/* Ticket Action Btn */}
-              <Box sx={{float:"right"}}>
-              <TicketBtn/>
-              </Box>
-            </Grid>
-          </Grid>
-        </Box>
+          </Box>
 
-        {/* Ticket details */}
-        <Box sx={{ padding: 1 }}>
-          {/* Inner Grey Box */}
-          <Box sx={{ padding: 1, bgcolor: `${ts.g300}`, borderRadius:1}}>
-            <Box>
-              <Typo_Basefont text="Details" fs="1.4rem" fw="600"/>
+          {/* Ticket details */}
+          <Box sx={{ padding: 1 }}>
+            {/* Inner Grey Box */}
+            <Box sx={{ padding: 1, bgcolor: `${ts.g300}`, borderRadius: 1 }}>
+              <Box>
+                <Typo_Basefont text="Details" fs="1.4rem" fw="600" />
+              </Box>
+
+              <Box>
+                <Box
+                  sx={{
+                    display: "inline-block",
+                    border: "2px solid #C700FF",
+                    padding: 0.6,
+                    borderRadius: 25,
+                    marginBottom: 1,
+                  }}
+                >
+                  <div className="flx aic">
+                    <Box sx={{ marginRight: 1 }}>
+                      <Imgbox url={beer} />
+                    </Box>
+                    <Box sx={{ paddingRight: 1 }}>
+                      <Typo_Chip text={"Test "} fw="400" />
+                    </Box>
+                  </div>
+                </Box>
+              </Box>
             </Box>
           </Box>
         </Box>
-      </Box>
       </Container>
     </>
   );
