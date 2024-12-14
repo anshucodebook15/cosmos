7b37177cf84a473bc3d07f0ce6ed5b1f77211431
Anshu Saxena <anshus5802@gmail.com>
2024-12-14 11:47:02 +0530

    maintanence mode on

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Landing from "./pages/Landing/Landing";
 import Details from "./pages/Details/Details";
 import { Box, Container } from "@mui/material";
 import AppInput from "./components/AppInput/AppInput";
+import Maintainance from "./components/Maintainance/Maintainance";
 
 function App() {
   return (
@@ -14,7 +15,10 @@ function App() {
         <Box sx={{ padding: 2, background: "#ca1e6b", position: "relative", color: "#fff", zIndex: 200 }}>
           <h5>Site is in maintainance mode ... ticket booking will be start soon </h5>
         </Box>
-        <Landing />
+
+        {/* <Maintainance /> */}
+    
+        {/* <Landing /> */}
       </div>
     </>
   );
---
+++ src/components/Maintainance/Maintainance.jsx
@@ -0,0 +1,16 @@
+
+import React from 'react'
+import './Maintainance.scss'
+import Container from '@mui/material/Container'
+
+const Maintainance = () => {
+  return (
+    <div className='Maintainance'>
+      <Container maxWidth="lg">
+        <h2>Website is in maintenance Mode... we will be back soon  </h2>
+      </Container>
+    </div>
+  )
+}
+
+export default Maintainance
