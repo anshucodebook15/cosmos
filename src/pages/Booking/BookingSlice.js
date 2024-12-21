5e16e04c361a2b3311f3cb1174470964c12859fb
Anshu Saxena <anshus5802@gmail.com>
2024-12-21 19:10:17 +0530

    test localhost

--- src/pages/Booking/BookingSlice.js
+++ src/pages/Booking/BookingSlice.js
@@ -10,7 +10,7 @@ export const COSBaseURL = "https://be.dunesofcosmos.com/";
 const API = axios.create({
   baseURL: COSBaseURL,
   headers: {
-    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Origin": "https://be.dunesofcosmos.com/",
   },
   withCredentials: true,
 });
